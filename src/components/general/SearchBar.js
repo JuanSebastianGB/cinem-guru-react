2a3fdbf7e6c0fc9bd748fceb6b752607c53ff9bd
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2022-11-17 20:52:09 -0500

    Fix input UI

--- src/components/general/SearchBar.js
+++ src/components/general/SearchBar.js
@@ -3,9 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './general.css';
 
 const SearchBar = ({ title, setTitle, dark }) => {
-  const handleInput = (e) => {
-    setTitle(e.target.value);
-  };
+  const handleInput = (e) => setTitle(e.target.value);
+
   return (
     <div className={`SearchBar ${dark ? 'searchbar-dark' : ''}`}>
       <FontAwesomeIcon icon={faSearch} />
